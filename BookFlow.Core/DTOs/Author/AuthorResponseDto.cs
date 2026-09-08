24f9dd98d6e52bc4776832c38ea6aacee998d028
Chiheb <chihebabiza@gmail.com>
2026-09-08 19:37:42 +0100

    auth dtos done

--- BookFlow.Core/DTOs/Author/AuthorResponseDto.cs
+++ BookFlow.Core/DTOs/Author/AuthorResponseDto.cs
@@ -3,4 +3,8 @@ namespace BookFlow.Core.DTOs;
 public class AuthorResponseDto
 {
     public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string CountryName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
 }
--- BookFlow.Core/DTOs/Author/AuthorUpdateDto.cs
+++ BookFlow.Core/DTOs/Author/AuthorUpdateDto.cs
@@ -2,4 +2,7 @@ namespace BookFlow.Core.DTOs;
 
 public class AuthorUpdateDto
 {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int CountryId { get; set; }
 }
