727ac403c830782f6572be4e81b395ca7e6e3693
Chiheb <chihebabiza@gmail.com>
2026-09-08 19:12:30 +0100

    book crud

--- BookFlow.Core/Interfaces/Book/IBookRepository.cs
+++ BookFlow.Core/Interfaces/Book/IBookRepository.cs
@@ -1,6 +1,19 @@
+using BookFlow.Core.Entities;
+using BookFlow.Core.Enums;
+
 namespace BookFlow.Core.Interfaces;
 
 public interface IBookRepository
 {
+    Task<IEnumerable<Book>> GetAllAsync();
 
+    Task<Book?> GetByIdAsync(int id);
+
+    Task<int> CreateAsync(Book book);
+
+    Task<Book?> GetByIdForUpdateAsync(int id);
+
+    Task<bool> UpdateAsync(Book book);
+
+    Task<DeleteResult> DeleteAsync(int id);
 }
--- BookFlow.DAL/Repositories/Book/BookRepository.cs
+++ BookFlow.DAL/Repositories/Book/BookRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using BookFlow.Core.Entities;
 using BookFlow.DAL.Context;
 using Microsoft.EntityFrameworkCore;
+using BookFlow.Core.Enums;
 
 namespace BookFlow.DAL.Repositories;
 
@@ -13,6 +14,59 @@ public class BookRepository : IBookRepository
     public BookRepository(AppDbContext context)
     {
         _context = context;
+    }
+
+    public async Task<IEnumerable<Book>> GetAllAsync()
+    {
+        return await _context.Books
+            .AsNoTracking()
+            .OrderBy(x => x.Id)
+            .ToListAsync();
+    }
+
+    public async Task<Book?> GetByIdAsync(int id)
+    {
+        return await _context.Books
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<int> CreateAsync(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+        return book.Id;
+    }
+
+    public async Task<Book?> GetByIdForUpdateAsync(int id)
+    {
+        return await _context.Books
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<bool> UpdateAsync(Book book)
+    {
+        _context.Books.Update(book);
+        var affectedRows = await _context.SaveChangesAsync();
+        return affectedRows > 0;
+    }
+
+    public async Task<DeleteResult> DeleteAsync(int id)
+    {
+        try
+        {
+            _context.Books.Remove(new Book { Id = id });
+            await _context.SaveChangesAsync();
+            return DeleteResult.Success;
+        }
+        catch (DbUpdateException ex) when (ex.InnerException is SqlException sqlEx && sqlEx.Number == 547)
+        {
+            return DeleteResult.HasDependencies;
+        }
+        catch (Exception)
+        {
+            return DeleteResult.SqlProblem;
+        }
     }
 
 }
